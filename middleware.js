16f59e23c24b18f135ca9514b01dcb90496fed75
nachiket mehta <nmehta@mediafly.com>
2015-10-09 15:46:14 -0500

    middleware

---
+++ middleware.js
@@ -0,0 +1,16 @@
+// var interactiveUrl = require('./package.json').interactiveUrl
+//
+// console.log('interactiveUrl', interactiveUrl)
+//
+// var exp = new RegExp('(.*)/interactive/(.*)/index.html')
+// var parts = interactiveUrl.match(exp)
+//
+// console.log(parts)
+
+var modRewrite = require('connect-modrewrite')
+var modRewriteConfig = [
+	'^(.*)/interactive/9cf282320e6340ee8b830e5376d54531product184614/1441745710d55945/(.*)$ /$2 [L]',
+	'^(.*)$ https://viewer.mediafly.com/$1 [P, L]'
+]
+
+module.exports = modRewrite(modRewriteConfig)
